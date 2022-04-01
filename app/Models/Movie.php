cd9562b81b492252cd753ab3613f171fc126ae3d
Oguz Topcu <oguztopcu7@gmail.com>
2022-04-01 03:02:27 +0300

    movie model

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int id
+ * @property string title
+ * @property string poster
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property Carbon deleted_at
+ */
 class Movie extends Model
 {
     use SoftDeletes, HasFactory;
