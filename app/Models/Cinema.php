709e94257bbc506f893c760b3ece05b1ae73bd88
Oguz Topcu <oguztopcu7@gmail.com>
2022-04-01 03:01:43 +0300

    cinema model

--- app/Models/Cinema.php
+++ app/Models/Cinema.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int id
+ * @property int city_id
+ * @property string title
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property Carbon deleted_at
+ * @property-read City city
+ */
 class Cinema extends Model
 {
     use SoftDeletes, HasFactory;
@@ -13,4 +25,14 @@ class Cinema extends Model
     protected $guarded = ['id'];
 
     protected $dates = ['deleted_at'];
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function movies(): BelongsToMany
+    {
+        return $this->belongsToMany(Movie::class, 'movie_cinemas');
+    }
 }
