9c37cf6a6b08874509fc965eea3157d342898ac3
Oguz Topcu <oguztopcu7@gmail.com>
2022-04-01 03:02:49 +0300

    ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int id
+ * @property int user_id
+ * @property int cinema_id
+ * @property int movie_id
+ * @property int number
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property Carbon deleted_at
+ */
 class Ticket extends Model
 {
     use SoftDeletes, HasFactory;
