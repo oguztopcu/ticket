c0dd9bd46ad2bbd0a1cc46593fcfb1e29aa20bcd
Oguz Topcu <oguztopcu7@gmail.com>
2022-04-01 03:01:49 +0300

    city model

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int id
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property Carbon deleted_at
+ * @property-read Cinema[] cinemas
+ */
 class City extends Model
 {
     use SoftDeletes, HasFactory;
@@ -13,4 +22,9 @@ class City extends Model
     protected $guarded = ['id'];
 
     protected $dates = ['deleted_at'];
+
+    public function cinemas(): HasMany
+    {
+        return $this->hasMany(Cinema::class);
+    }
 }
